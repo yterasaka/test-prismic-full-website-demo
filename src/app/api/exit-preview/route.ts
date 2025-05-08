6ba991531015856ce91937c0f8d716afaf26c69e
Yuki Terasaka <yuki.terasaka@gmail.com>
2025-05-08 09:56:00 +0900

    Initial CP setting

--- src/app/api/exit-preview/route.ts
+++ src/app/api/exit-preview/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import { exitPreview } from "@prismicio/next";
 
 /**
--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import { NextRequest } from "next/server";
 import { redirectToPreviewURL } from "@prismicio/next";
 
